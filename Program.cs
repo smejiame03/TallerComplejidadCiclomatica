823542463c8452e16f3cbb8c6b88af87caf347c6
saraymejiame <saray.mejiame@amigo.edu.co>
2022-02-21 19:36:19 -0500

    TallerComplejidad1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Taller_ComplejidadCiclo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Ingrese el número a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número b: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a>b)
+            {
+                Console.WriteLine("El número a: ", a, "es mayor que el número b: ", b);
+            }
+            else
+            {
+                if(a==b)
+                {
+                    Console.WriteLine("El número a: ", a, "es igual al número b: ", b);
+                }
+                else
+                {
+                    Console.WriteLine("El número a: ", a, "es menor que el número b: ", b);
+                }
+            }
+        }
+    }
+}
